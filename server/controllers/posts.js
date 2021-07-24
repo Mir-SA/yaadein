f94cd756c2e509ac0a19126595e6e8095f7d424d
Mir-SA <kumarvij0202@gmail.com>
2021-07-24 11:26:28 +0530

    added new routes & bug fixes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 // @desc    Get all posts
 // @route   GET /posts
@@ -27,4 +28,52 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+};
+
+// @desc    Update a post
+// @route   PATCH /posts/:id
+// @access  Public
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("Post does not exist");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
+};
+
+// @desc    Delete a post
+// @route   DELETE /posts/:id
+// @access  Public
+export const delPost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send("Post does not exist");
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully" });
+};
+
+// @desc    Like a post
+// @route   PATCH /posts/:id
+// @access  Public
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send("Post does not exist");
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+        id,
+        { likeCount: post.likeCount + 1 },
+        { new: true }
+    );
+
+    res.json(updatedPost);
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +15,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const DB_URI =
-    "mongodb+srv://mir:mir123@cluster0.iwdxn.mongodb.net/DB?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 4000;
 
 mongoose
-    .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(`Error: ${error.message}`));
 
